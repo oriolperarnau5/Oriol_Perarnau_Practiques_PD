eda83a69cc0222596f6be082195297951e7f1f06
Oriol Perarnau <oriol.perarnau@estudiantat.upc.edu>
2025-03-11 12:28:22 +0100

    Codi 4.1 Practica 1

---
+++ Laboratori_1/Oriol_Perarnau_Practica_1_codi4_1/src/codi_4_1.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#define LED_PIN 48  // Cambia este pin por uno disponible en tu ESP32-S3
+void setup() {
+   pinMode(LED_PIN, OUTPUT);
+   Serial.begin(115200);
+}
+// 1. Con Serial y usando funciones de Arduino
+void loop() {
+   Serial.println("ON");
+   digitalWrite(LED_PIN, HIGH);
+   Serial.println("OFF");
+   digitalWrite(LED_PIN, LOW);
+}
